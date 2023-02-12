7eb8e5d8bb1c47c7b3d324ad4c3c3726d8e52a11
Hetav Patel <866639@pdsb.net>
2023-02-11 22:16:36 -0500

    added comment in dashboard

--- clubmanager/routes/dashboard.py
+++ clubmanager/routes/dashboard.py
@@ -21,12 +21,18 @@ def load_user(user_id):
 # Create routes
 @app.route('/login/dashboard', methods=['GET'])
 def get_login():
+    # initialize form
     form = LoginForm()
+
+    # render login page
     return render_template('login.html', form=form)
 
 @app.route('/login/dashboard', methods=['POST'])
 def create_login():
+    # initialize form
     form = LoginForm()
+
+    # check if the user exists in the database
     if form.validate_on_submit():
         user = Student.query.filter_by(StudentNum=form.StudentNum.data).first()
         if user:
@@ -34,21 +40,31 @@ def create_login():
                 login_user(user, remember=True)
                 return redirect(url_for('dashboard'))
         flash('Incorrect Username or Password!', 'error') #warning, info, error
+
+        # render login page
         return render_template('login.html', form=form)
 
 @app.route('/register/dashboard', methods=['GET'])
 def get_registration():
+    # initialize form
     form = RegisterForm()
+
+    # initialize blank errors list
     errors = ['', '', '']
+
+    # render registration page
     return render_template('register.html', form=form, errors=errors)
 
 @app.route('/register/dashboard', methods=['POST'])
 def create_user():
+    # initialize form and other variables associated with checking the validity of the registration information
     form = RegisterForm()
     checkifusernameisunique = Student.query.filter_by(Username=form.Username.data).first()
     checkifstudentnumisunique = Student.query.filter_by(StudentNum=form.StudentNum.data).first()
     checkifemailisunique = Student.query.filter_by(Email=form.Email.data).first()
     errors = ['', '', '']
+
+    # check the validity of the registration information entered and display errors accordingly
     if checkifusernameisunique != None:
         errors[0] = ('Username is already taken')
     if checkifstudentnumisunique != None:
@@ -56,6 +72,8 @@ def create_user():
     if checkifemailisunique != None:
         errors[2] = ('Email already exists')
     condition = checkifusernameisunique == None and checkifstudentnumisunique == None and checkifemailisunique == None
+
+    # create a new user if registration information is valid and hide password for security reasons
     if form.validate_on_submit():
         if condition:
             hashed_Password = generate_password_hash(form.Password.data, method='sha256')
@@ -64,19 +82,31 @@ def create_user():
             db.session.commit()
             login_user(new_user, remember=True)
             return redirect(url_for('dashboard'))
+    
+    # render registration page
     return render_template('register.html', form=form, errors=errors)
 
 @app.route('/dashboard')
 @login_required
 def dashboard():
+    # get all the clubs the user owns to display on their dashboard
     userClubCatalogue = getUserOwnedClubs(current_user.StudentNum)
+
+    # initialize variable
     truthy = True
+
+    # set truthy as False if the user owns clubs
     if userClubCatalogue:
         truthy = False
+    
+    # render dashboard page
     return render_template('dashboard.html', truthy=truthy, name=current_user.FirstName, userClubCatalogue=userClubCatalogue) 
 
 @app.route('/logout')
 @login_required
 def logout():
+    # logout the current user if they click logout
     logout_user()
+
+    # redirect to the landing page
     return redirect(url_for('index'))
